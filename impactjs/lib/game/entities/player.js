6ff9a281cad5286b1f04fcf24292bb76588c8185
Marty Penner <marty@gotenzing.com>
2012-07-04 20:41:30 -0400

    Update the player entity sprite sizes and add diagonal movement

--- impactjs/lib/game/entities/player.js
+++ impactjs/lib/game/entities/player.js
@@ -7,47 +7,93 @@ ig.module(
 .defines(function () {
     EntityPlayer = ig.Entity.extend({
 
-        size: {x: 64, y: 64},
+        size: {x: 48, y: 64},
         collides: ig.Entity.COLLIDES.ACTIVE,
 
-        animSheet: new ig.AnimationSheet('media/hero.png', 64, 64),
+        animSheet: new ig.AnimationSheet('media/hero.png', 48, 64),
+
+        // Last direction the player was facing, so the correct idle animation is shown
+        lastDirection: 'Down',
+
+        // Default moving velocity
+        velocity: 200,
+
+        // Maximum velocity
+        maxVel: {x: 500, y: 500},
+
+        idleAnimSpeed: 1,
+        movingAnimSpeed: 0.06,
+
+        // Whether the entity is allowed to move
+        movementAllowed: true,
 
         init: function (x, y, settings) {
             // Add animations for the animation sheet
-            this.addAnim('idle', 0.1, [0]);
-            this.addAnim('walkDown', 0.1, [0, 1, 2, 3, 4, 5, 6, 7]);
-            this.addAnim('walkUp', 0.1, [8, 9, 10, 11, 12, 13, 14, 15]);
-            this.addAnim('walkRight', 0.1, [16, 17, 18, 19, 20, 21, 22, 23]);
-            this.addAnim('walkLeft', 0.1, [24, 25, 26, 27, 28, 29, 30, 31]);
+            this.addAnim('idleDown', this.idleAnimSpeed, [0]);
+            this.addAnim('idleUp', this.idleAnimSpeed, [8]);
+            this.addAnim('idleRight', this.idleAnimSpeed, [16]);
+            this.addAnim('idleLeft', this.idleAnimSpeed, [24]);
+            this.addAnim('walkDown', this.movingAnimSpeed, [0, 1, 2, 3, 4, 5, 6, 7]);
+            this.addAnim('walkUp', this.movingAnimSpeed, [8, 9, 10, 11, 12, 13, 14, 15]);
+            this.addAnim('walkRight', this.movingAnimSpeed, [16, 17, 18, 19, 20, 21, 22, 23]);
+            this.addAnim('walkLeft', this.movingAnimSpeed, [24, 25, 26, 27, 28, 29, 30, 31]);
 
             // Call the parent constructor
             this.parent(x, y, settings);
         },
 
         update: function () {
+            // Check for button presses and activate the appropriate animation
+            this.handleButtons();
+
+            // Call the parent to get physics and movement updates
+            this.parent();
+        },
+
+        handleButtons: function () {
+            if (!this.movementAllowed) {
+                return;
+            }
+
             if (ig.input.state('up')) {
                 this.currentAnim = this.anims.walkUp;
+                this.lastDirection = 'Up';
                 this.vel.x = 0;
-                this.vel.y = -100;
+                this.vel.y = -this.velocity;
+
+                if (ig.input.state('right')) {
+                    this.vel.x = this.velocity;
+                } else if (ig.input.state('left')) {
+                    this.vel.x = -this.velocity;
+                }
             } else if (ig.input.state('down')) {
                 this.currentAnim = this.anims.walkDown;
+                this.lastDirection = 'Down';
                 this.vel.x = 0;
-                this.vel.y = 100;
+                this.vel.y = this.velocity;
+
+                if (ig.input.state('right')) {
+                    this.vel.x = this.velocity;
+                } else if (ig.input.state('left')) {
+                    this.vel.x = -this.velocity;
+                }
             } else if (ig.input.state('right')) {
                 this.currentAnim = this.anims.walkRight;
-                this.vel.x = 100;
+                this.lastDirection = 'Right';
+                this.vel.x = this.velocity;
                 this.vel.y = 0;
             } else if (ig.input.state('left')) {
                 this.currentAnim = this.anims.walkLeft;
-                this.vel.x = -100;
+                this.lastDirection = 'Left';
+                this.vel.x = -this.velocity;
                 this.vel.y = 0;
             } else {
-                this.currentAnim = this.anims.idle;
+                // Stop all movement and show the correct idle animation for
+                // the direction the player is facing
+                this.currentAnim = this.anims['idle' + this.lastDirection];
                 this.vel.x = 0;
                 this.vel.y = 0;
             }
-
-            this.parent();
         }
     });
 });
